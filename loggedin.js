c0beb97d0da8ca2fb3485afd043361e7abf7b623
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-19 08:23:39 -0400

    Modularize code by creating a helper function create_table

--- loggedin.js
+++ loggedin.js
@@ -4,10 +4,7 @@ logoutButton.addEventListener("click", logout);
 var username = sessionStorage.getItem("USERNAME");
 
 let allReimbursementsButton = document.querySelector("#finance_manager button");
-allReimbursementsButton.addEventListener(
-  "click",
-  finance_manager_get_all_reimbursement
-);
+allReimbursementsButton.addEventListener("click", get_all_reimbursement);
 
 async function logout() {
   let res = await fetch(`http://127.0.0.1:8080/logout`, { method: "POST" });
@@ -17,7 +14,7 @@ async function logout() {
   }
 }
 
-async function finance_manager_get_all_reimbursement() {
+async function get_all_reimbursement() {
   let res = await fetch(`http://127.0.0.1:8080/users/${username}`, {
     credentials: "include",
     method: "GET",
@@ -25,6 +22,39 @@ async function finance_manager_get_all_reimbursement() {
   let data = await res.json();
   let data_array = data[`Reimbursement details of ${username}`];
 
+  create_table(data_array);
+}
+
+window.addEventListener("load", () => {
+  const fullName = sessionStorage.getItem("FULLNAME");
+  const role = sessionStorage.getItem("ROLE");
+  const username = sessionStorage.getItem("USERNAME");
+
+  if (!username) {
+    window.location.href = "/index.html";
+  }
+
+  let name = document.querySelector("#name");
+  let role1 = document.querySelector("#role");
+  let username1 = document.querySelector("#username");
+  name.innerHTML = fullName;
+  role1.innerHTML = role;
+  username1.innerHTML = username;
+
+  if (role != "finance_manager") {
+    let employee_create_reimbursement = document.querySelector(
+      "#employee_create_reimbursement a"
+    );
+    employee_create_reimbursement.setAttribute("href", "");
+    employee_create_reimbursement.setAttribute(
+      "class",
+      "button is-danger is-outlined"
+    );
+    employee_create_reimbursement.innerHTML = "Create Reimbursement";
+  }
+});
+
+function create_table(data_array) {
   let tableBody = document.querySelector("#t-body");
 
   // check if the tbody already has any child elements. If any child elements found, don't create a new table
@@ -79,32 +109,3 @@ async function finance_manager_get_all_reimbursement() {
     }
   }
 }
-
-window.addEventListener("load", () => {
-  const fullName = sessionStorage.getItem("FULLNAME");
-  const role = sessionStorage.getItem("ROLE");
-  const username = sessionStorage.getItem("USERNAME");
-
-  if (!username) {
-    window.location.href = "/index.html";
-  }
-
-  let name = document.querySelector("#name");
-  let role1 = document.querySelector("#role");
-  let username1 = document.querySelector("#username");
-  name.innerHTML = fullName;
-  role1.innerHTML = role;
-  username1.innerHTML = username;
-
-  if (role != "finance_manager") {
-    let employee_create_reimbursement = document.querySelector(
-      "#employee_create_reimbursement a"
-    );
-    employee_create_reimbursement.setAttribute("href", "");
-    employee_create_reimbursement.setAttribute(
-      "class",
-      "button is-danger is-outlined"
-    );
-    employee_create_reimbursement.innerHTML = "Create Reimbursement";
-  }
-});
