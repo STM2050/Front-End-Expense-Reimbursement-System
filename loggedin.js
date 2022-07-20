34976ea5c6da941d46a4f672231c815dc8048e73
Bipul Chaudhary <bipulchau@gmail.com>
2022-07-20 18:10:56 -0400

    Display approve and reject button for the pending reimbursements

--- loggedin.js
+++ loggedin.js
@@ -106,7 +106,24 @@ function create_table(data_array) {
 
       let type_of_expense = document.createElement("td");
       type_of_expense.innerHTML = user["type_of_expense"];
-      tableRow.appendChild(type_of_expense);
+      tableRow.appendChild(type_of_expense); //.********************************************
+
+      let approve_deny = document.createElement("td");
+      let approve_btn = document.createElement("button");
+      approve_btn.setAttribute("type", "button");
+      approve_btn.setAttribute("name", user["reimb_id"]);
+      approve_btn.setAttribute("value", "approved");
+      approve_btn.textContent = "Approve";
+      let deny_btn = document.createElement("button");
+      deny_btn.setAttribute("type", "button");
+      deny_btn.setAttribute("name", user["reimb_id"]);
+      deny_btn.setAttribute("value", "denied");
+      deny_btn.textContent = "Deny";
+      if (status.innerHTML == "pending" && role == "finance_manager") {
+        approve_deny.appendChild(approve_btn);
+        approve_deny.appendChild(deny_btn);
+        tableRow.appendChild(approve_deny);
+      }
     }
   } else {
     {
@@ -114,51 +131,56 @@ function create_table(data_array) {
       for (let user of data_array) {
         let tableRow = document.createElement("tr");
         tableRow.setAttribute("id", "reimb_row");
-
         tableBody.appendChild(tableRow);
-
         let description = document.createElement("td");
         description.innerHTML = user["description"];
         tableRow.appendChild(description);
-
         let receipt_img = document.createElement("td");
         let img = document.createElement("img");
         img.setAttribute("src", user["receipt_img"]);
         receipt_img.appendChild(img);
         tableRow.appendChild(receipt_img);
-
         let reimb_author = document.createElement("td");
         reimb_author.innerHTML = user["reimb_author"];
         tableRow.appendChild(reimb_author);
-
         let reimb_id = document.createElement("td");
         reimb_id.innerHTML = user["reimb_id"];
         tableRow.appendChild(reimb_id);
-
         let reimb_resolver = document.createElement("td");
         reimb_resolver.innerHTML = user["reimb_resolver"];
         tableRow.appendChild(reimb_resolver);
-
         let reimbursement_amount = document.createElement("td");
         reimbursement_amount.innerHTML = user["reimbursement_amount"];
         tableRow.appendChild(reimbursement_amount);
-
         let resolved_at = document.createElement("td");
         resolved_at.innerHTML = user["resolved_at"];
         tableRow.appendChild(resolved_at);
-
         let status = document.createElement("td");
         status.innerHTML = user["status"];
-
         tableRow.appendChild(status);
-
         let submitted_at = document.createElement("td");
         submitted_at.innerHTML = user["submitted_at"];
         tableRow.appendChild(submitted_at);
-
         let type_of_expense = document.createElement("td");
         type_of_expense.innerHTML = user["type_of_expense"];
         tableRow.appendChild(type_of_expense);
+
+        let approve_deny = document.createElement("td");
+        let approve_btn = document.createElement("button");
+        approve_btn.setAttribute("type", "button");
+        approve_btn.setAttribute("name", user["reimb_id"]);
+        approve_btn.setAttribute("value", "approved");
+        approve_btn.textContent = "Approve";
+        let deny_btn = document.createElement("button");
+        deny_btn.setAttribute("type", "button");
+        deny_btn.setAttribute("name", user["reimb_id"]);
+        deny_btn.setAttribute("value", "denied");
+        deny_btn.textContent = "Deny";
+        if (status.innerHTML == "pending" && role == "finance_manager") {
+          approve_deny.appendChild(approve_btn);
+          approve_deny.appendChild(deny_btn);
+          tableRow.appendChild(approve_deny);
+        }
       }
     }
   }
@@ -169,24 +191,24 @@ var text = ""; // declared globally in order to use it in the function filter_re
 selectElement.addEventListener("change", (e) => {
   text = e.target.value;
   console.log(text);
-  let reimbursement_table = document.querySelector("#table_reimbursement");
-  let num_of_column_of_table = reimbursement_table.rows[0].cells.length;
-  if (text == "pending" && role == "finance_manager") {
-    // create action column if the status is selected as pending and the user's role is finance_manager
-    if (num_of_column_of_table == 10) {
-      let table_head_row = document.querySelector("#table-head-row");
-      let action_column = document.createElement("th");
-      action_column.setAttribute("id", "action_column");
-      action_column.textContent = "Action";
-      table_head_row.appendChild(action_column);
-    }
-  } else {
-    // delete action column if status is selected as denied or approved
-    if (num_of_column_of_table > 10) {
-      let action_column = document.querySelector("#action_column");
-      action_column.remove();
-    }
-  }
+  // let reimbursement_table = document.querySelector("#table_reimbursement");
+  // let num_of_column_of_laterTable = reimbursement_table.rows[0].cells.length;
+  // if (text == "pending" && role == "finance_manager") {
+  //   // create action column if the status is selected as pending and the user's role is finance_manager
+  //   if (num_of_column_of_laterTable == 10) {
+  //     let table_head_row = document.querySelector("#table-head-row");
+  //     let action_column = document.createElement("th");
+  //     action_column.setAttribute("id", "action_column");
+  //     action_column.textContent = "Action";
+  //     table_head_row.appendChild(action_column);
+  //   }
+  // } else {
+  //   // delete action column if status is selected as denied or approved
+  //   if (num_of_column_of_laterTable > 10) {
+  //     let action_column = document.querySelector("#action_column");
+  //     action_column.remove();
+  //   }
+  // }
 });
 
 selectElement.addEventListener("change", filter_reimbursement_status);
